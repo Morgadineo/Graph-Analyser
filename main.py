67f10c95f29764e0acf8af2678b8a1fe898f9dd8
Morgadineo <arthurmorgado7751@gmail.com>
2025-03-20 20:25:01 -0300

    Mega update

--- main.py
+++ main.py
@@ -4,10 +4,11 @@
         b. [ ] Informe se o grafo é uma arvore, se for, informe o tipo
         c. [X] Informe o grau de cada vértice
         d. [X] Informe o grau o grafo
-        e. [X] Informe se o grafo é completo
+        e. [x] Informe se o grafo é completo
         f. [X] Informe se o grafo possui laços
-        g. [X] Informe se é um grafo simples
+        g. [x] Informe se é um grafo simples
         h. [X] Imprima a matriz/lista de adjacência
+        i. [X] Verificar se o grafo é simétrico, assimétrico / não simétrico
 
 """
 
@@ -23,27 +24,121 @@ class Graph:
         self.edges_qtt: int      = self.count_edges()                         # Quantidade de arestas
         
         # Atributos de verificação
-        self.has_loop: bool     = self.verify_loop()
+        self.has_loop    : bool = self.verify_loop()
         self.has_parallel: bool = self.verify_parallel()
        
         # Se has_loop ou has_parallel forem verdadeiros, o grafo não é simples/completo.
-        self.its_simple: bool   = self.verify_simplicity()
-        self.its_complete: bool = self.verify_completeness()
-        self.its_tree: bool     = True  # Considera que é uma até não ser.
+        self.its_simple   : bool = self.verify_simplicity()
         
         # Contagem dos graus de entrada e saída de cada vértice.
         self.vertices_out_degree: list[int] = self.count_vertices_out_degree()
-        self.vertices_in_degree: list[int]  = self.count_vertices_in_degree()
+        self.vertices_in_degree : list[int]  = self.count_vertices_in_degree()
         
         # Contagem de graus de entrada e saída do grafo.
         self.graph_out_degree: int = self.count_graph_out_degree()
-        self.graph_in_degree: int  = self.count_graph_in_degree()
+        self.graph_in_degree : int  = self.count_graph_in_degree()
 
         # Matriz de adjacência
         self.adjacency_matrix: list[int] = self.create_adjacency_matrix()
         
         self.print_adjancency_matrix()
 
+        # Caracteristicas do Digrafo
+        self.its_simetry           : bool = self.verify_simetry()
+        self.its_assimetry         : bool = self.verify_assimetry() 
+        self.its_complete          : bool = self.verify_completeness()
+        self.its_complete_simetry  : bool = self.its_complete and self.its_simetry
+        self.its_complete_assimetry: bool = self.its_complete and self.its_assimetry
+        
+        self.its_balanced          : bool = self.verify_balancing()
+        # self.its_connected         : bool = self.verify_connectivity()
+        
+    def verify_balancing(self) -> bool:
+        """
+        Verify if the digraph is balanced.
+        A digraph is balanced if every vertice has the same out and in degree.
+
+        return:
+            A boolean value. True: if the digraph is balance; False: if is not.
+        """
+        for i in range(self.vertices_qtt):
+            if (self.vertices_in_degree[i] != self.vertices_out_degree[i]):
+                return False
+            
+        return True
+
+    def verify_completeness(self) -> bool:
+        """
+        Verify if the digraph is complete.
+        A complete digraph is a simple digraph where every vertice has at least one edge with all the other.
+
+        return:
+            A boolean value. True: If the graph is complete; False: if is not.
+        """
+        has_edge: bool = False
+        if self.its_simple:
+            for i in range(self.vertices_qtt):
+                has_edge = False
+                for j in range(self.vertices_qtt):
+                    if i != j:
+                        if (self.adjacency_matrix[j][i] or self.adjacency_matrix[i][j]):
+                            has_edge = True
+            if not has_edge:
+                return False
+            else:
+                return True
+        else:
+            return False
+
+    def verify_assimetry(self) -> bool:
+        """
+        Verify if the digraph is assimetry.
+        A digraph is a assimetry digraph if have at most one arc direction between two vertices.
+
+
+        return:
+            A boolean value. True: if is assimetry; False: if is not.
+        """
+        if not (self.its_simetry):
+            for i in range(self.vertices_qtt):
+                for j in range(self.vertices_qtt):
+                    if i != j: # Se não estiver na diagonal principal:
+                        if (self.adjacency_matrix[j][i]) and (self.adjacency_matrix[i][j]):
+                            return False
+            return True
+        return False
+
+    def verify_simetry(self) -> bool:
+        """
+        Verify if the digraph is simetry.
+        A digraph is a simetry digraph if for every arc(a, b) exist a arc(b, c).
+
+        return:
+            A boolean value. True: the digraph is simetry; False: it is not.
+        """
+        for i in range(self.vertices_qtt):
+            for j in range(self.vertices_qtt):
+                if i != j: # Se não estiver na diagonal principal
+                    # Se existir uma aresta entre (i, j) e não tiver uma entre (j, i)
+                    if (self.adjacency_matrix[i][j]) and (not (self.adjacency_matrix[j][i])):
+                        return False # Não é simétrica
+        return True # Não tenhuma que liga (i, j) e não liga (j, i) -> é simétrica.
+
+    def verify_is_tree(self) -> bool:
+        """
+        Identify if the graph is a tree.
+        To identify, is verify if the graph is simple, and if is, verify if the
+        number of edges are the number of vertices minus one.
+
+        return:
+            A boolean value. True: the graph is a tree; False is not.
+        """
+        if self.its_simple:
+            if self.edges_qtt == (self.vertices_qtt) - 1:
+                return True
+        else:
+            return False
+>>>>>>> 610f642 (Mega update)
 
     def print_adjancency_matrix(self) -> None:
 
@@ -171,19 +266,6 @@ class Graph:
                     print(adj_vertice)
                 else:
                     print(f"{adj_vertice}, ", end="")
-         
-    def verify_completeness(self) -> bool:
-        """
-        Verify if the graph is complete or not.
-        A complete graph is a graph where all the vertices are connect with the others.
-
-        Returns:
-            True if is complete and False if is not.
-        """
-        if self.its_simple:
-            if self.edges_qtt == (self.vertices_qtt * (self.vertices_qtt - 1)):
-                return True
-        return False
 
     def verify_simplicity(self) -> bool:
         """
@@ -238,14 +320,32 @@ class Graph:
 
         return edges_qtt
 
+def create_graph() -> dict[str: list[str]]:
+    """
+    Function to create a adjancency list of a graph.
+        1º Input the number of vertices.
+        2º Input the vertice name. Example: A
+        3º Input the vertices to create a edge with. Example: A B C
+    
+    return:
+        A dict {vertice: [v1, v2, v3]} with the vertice as key and a list of edges as value. 
+    """
+    number_vertices: int = int(input("Quantos vértices terá? "))
+    adjacency_list = {} 
+    for vertice in range(number_vertices):
+        vertice = input("Vertice: ")
+        edges   = input("Arestas: ")
+         
+        edges = edges.split(' ')
+        print('\n')
+
+        adjacency_list.update({vertice: edges})
+    return adjacency_list
+
 if __name__ == '__main__':
     # Representação de um grafo:
     # {'Vértice': ['array de vértices conectados']}
     # Caso o vértice não possua conexões, fica:
     # {'Vértice': []}
-    graph = Graph({'A': ['B', 'C'], 
-                   'B': [],
-                   'C': ['B'],
-                   'D': ['B', 'B', 'D']})
-    
+    graph = Graph(create_graph())
 
